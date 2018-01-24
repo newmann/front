0225d0f2b21e7974066b0b17d47b82b2be63d0e0
newmann <newmannhu@qq.com>
2018-01-24 17:35:42 +0800

    20180124 测试

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -22,7 +22,6 @@ import {StoreDevtoolsModule} from "@ngrx/store-devtools";
 import {reducers,metaReducers} from "./reducer/index";
 import {CustomRouterStateSerializer} from "./shared/routerstate.utils";
 import {HttpClientModule, HTTP_INTERCEPTORS} from "@angular/common/http";
-import {StompRService} from "@stomp/ng2-stompjs";
 import {CustomeHttpInterceptor} from "./core/custome-http.interceptor";
 
 // import { FireBaseComponentsModule } from './shared/firebase.module';
@@ -93,12 +92,7 @@ import {CustomeHttpInterceptor} from "./core/custome-http.interceptor";
      * by `@ngrx/router-store` to include only the desired pieces of the snapshot.
      */
     { provide: RouterStateSerializer, useClass: CustomRouterStateSerializer },
-    /**
-     * The initialization process can be manually controlled with the additional class
-     * StompRService which is injected instead of StompService. This has a few additional
-     * ethods to assign a configuration and manually initiate the connection to the STOMP Broker.
-     */
-    StompRService
+
   ],
   bootstrap: [AppComponent]
 })
--- web/src/app/core/websocket.service.ts
+++ web/src/app/core/websocket.service.ts
@@ -1,16 +1,21 @@
 import { Injectable } from '@angular/core';
-import {StompConfig, StompRService, StompState} from "@stomp/ng2-stompjs";
 import {Observable} from "rxjs/Observable";
 import "rxjs/add/operator/map";
 import {Message} from '@stomp/stompjs';
+import {AuthDataService} from "./auth-data.service";
+import * as SockJS from "sockjs-client";
+import * as Stomp from "@stomp/stompjs";
 
 @Injectable()
 export class WebsocketService {
-  config: StompConfig;
   websocketState: Observable<String>;
   generalMessage: Observable<Message>;
 
-  constructor(private _stompService: StompRService) {
+  socketjs: any = null;
+  stompClient: any = null;
+
+  constructor(private authData: AuthDataService) {
+  // private _stompService: StompRService,
     // console.log('Stomp init');
     // this.initStomp();
     // console.log('Status init');
@@ -23,34 +28,20 @@ export class WebsocketService {
 
   public initStomp() {
 
+  if (this.socketjs == null) {
+    this.socketjs = new SockJS("http://localhost:8090/beiyelin");
+  }
+  this.stompClient = Stomp.over(this.socketjs);
+  this.stompClient.connect({},
+    frame =>{console.log('Connected: ' + frame);},
+    err=>{console.log('err',err);},
+  );
 
-    this.config = {
-      // Which server?
-      url: 'ws://localhost:8090/beiyelin',
-
-      // Headers
-      // Typical keys: login, passcode, host
-      headers: {
-         login: 'guest',
-      //   passcode: 'guest'
-      },
-      // How often to heartbeat?
-      // Interval in milliseconds, set to 0 to disable
-      heartbeat_in: 0, // Typical value 0 - disabled
-      heartbeat_out: 20000, // Typical value 20000 - every 20 seconds
-      // Wait in milliseconds before attempting auto reconnect
-      // Set to 0 to disable
-      // Typical value 5000 (5 seconds)
-      reconnect_delay: 5000,
-      // Will log diagnostics on console
-      debug: true
-    };
-
-    this._stompService.config = this.config;
-    this._stompService.initAndConnect();
-    this.websocketState = this._stompService.state
-       .map((state: number) => StompState[state]);
-    this.generalMessage = this._stompService.subscribe('/topic');
+    // this._stompService.config = this.config;
+    // this._stompService.initAndConnect();
+    // this.websocketState = this._stompService.state
+    //    .map((state: number) => StompState[state]);
+    // this.generalMessage = this._stompService.subscribe('/topic');
 
   }
 
